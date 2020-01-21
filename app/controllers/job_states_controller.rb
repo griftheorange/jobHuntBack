a11cd259b876ce543d166d888e63f19b501a9d1f
Griffin Poole <griffinpoole5@gmail.com>
2020-01-21 15:17:24 -0500

    show functionality complete

--- app/controllers/job_states_controller.rb
+++ app/controllers/job_states_controller.rb
@@ -3,4 +3,9 @@ class JobStatesController < ApplicationController
         @job_states = JobState.all
         render json: @job_states
     end
+
+    def show
+        @job = Job.find_by(id: params[:id])
+        render json: @job
+    end
 end
