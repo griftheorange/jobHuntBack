039918a4013b3978c81e80042e51ca349b9d0739
Griffin Poole <griffinpoole5@gmail.com>
2020-01-21 21:21:20 -0500

    login crezate account validation complete for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
     has_secure_password
     has_many :job_states
     has_many :jobs, through: :job_states
+
+    validates :username, uniqueness: {message: "must be unique"}
+    validates :username, presence: {message: "can't be blank"}
+    validates :password, presence: {message: "can't be blank"}
 end
