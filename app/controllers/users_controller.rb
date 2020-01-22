0f879993c7876478529580b84172c91ef5451049
Griffin Poole <griffinpoole5@gmail.com>
2020-01-22 09:49:51 -0500

    token gen and response works for user create and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+    require 'jwt'
+
     def index
         @users = User.all
         render json: @users
@@ -10,15 +13,35 @@ class UsersController < ApplicationController
     end
 
     def login
-        render json: {yeet:"yeet"}
+        @user = User.find_by(username: params[:username])
+        if @user
+            if @user.authenticate(params["password"])
+                render json: {token: genToken(@user)}
+            else
+                render json: {errors: ["Password incorrect"]}
+            end
+            
+        else
+            render json: {errors: ["Did not find a user matching that username"]}
+        end
     end
 
     def create
         @user = User.create(username: params[:username], password: params[:password])
         if @user.valid?
-            render json: @user
+            render json: {token: genToken(@user)}
         else 
-            render json: @user.errors
+            render json: {errors: @user.errors.full_messages}
         end
+    end
+
+    private
+    def hmac_secret
+        "yeetOnTheHaters"
+    end
+
+    def genToken(user)
+        payload = {id: @user.id, type: "user"}
+        token = JWT.encode payload, hmac_secret, "HS256"
     end
 end
