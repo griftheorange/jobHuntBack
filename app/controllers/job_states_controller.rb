512694258c0b1493f4bc65f90f622d168762d4bb
Griffin Poole <griffinpoole5@gmail.com>
2020-01-22 21:08:48 -0500

    changes

--- app/controllers/job_states_controller.rb
+++ app/controllers/job_states_controller.rb
@@ -3,4 +3,9 @@ class JobStatesController < ApplicationController
         @job_states = JobState.all
         render json: @job_states
     end
+
+    def create
+        @job_state = JobState.create(user_id: params[:user_id], job_id: params[:job_id], status: params[:status])
+        render json: @job_state
+    end
 end
--- app/models/job_state.rb
+++ app/models/job_state.rb
@@ -2,6 +2,8 @@ class JobState < ApplicationRecord
     belongs_to :user
     belongs_to :job
 
+    validates :job_id, uniqueness: {scope: :user_id}
+
     # has_one :jobs
     # has_many :companies, through: :jobs
 end
