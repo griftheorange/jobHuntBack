ca0be21918227e10c8a5a67b9d3983742bcce90f
Amra Sezairi <amra.sezairi@hotmail.com>
2020-01-22 21:07:16 -0500

    jobs create

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,4 +8,30 @@ class JobsController < ApplicationController
         @job = Job.find_by(id: params[:id])
         render json: @job
     end
+
+
+
+    def create 
+        @job = Job.create(job_params)
+        if @job.valid?
+            render json: @job
+        else 
+            render json: {errors: @job.errors.full_messages} 
+        end
+    end   
+
+    private
+
+
+    def job_params
+        # params.require(:job).permit(:title,:company_id, :description, :companyName)
+        return {
+            title: params[:title],
+            company_id: params[:company_id],
+            post_date: Time.now,
+            description: params[:description],
+            companyName: params[:companyName]
+        }
+    end    
+
 end
